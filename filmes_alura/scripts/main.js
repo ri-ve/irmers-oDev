64f596cbaa3dc1146a26420852ff7ece79972ca2
RIVELINO SANTOS <80266863+ri-ve@users.noreply.github.com>
2021-09-16 11:40:36 -0300

    modelo netFlix so com Html css e javaScript

---
+++ filmes_alura/scripts/main.js
@@ -0,0 +1,19 @@
+var listaFilmes = ["https://th.bing.com/th/id/R.8b49046821bce0ccc8f291eee6fc3edf?rik=exIuHkcRD5vnww&riu=http%3a%2f%2fbr.web.img3.acsta.net%2fpictures%2f16%2f11%2f21%2f15%2f29%2f457312.jpg&ehk=g0pSrKgqt9gnzwLEvamA0tSVnaoIft25yrxLwe3GMPo%3d&risl=&pid=ImgRaw&r=0", "https://th.bing.com/th/id/R.fecd2d202c3ec25461d36a0c91f80fad?rik=r2D5NW8jcau5sw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-WBDK5v3HVBc%2fU20TGeUcCMI%2fAAAAAAAAA3k%2f0R9_hTATLIw%2fs1600%2fO-Livro-de-Eli-poster.jpg&ehk=44vM%2b8hE0x13%2bTArh65W2nD1k1HDe%2fkrc7GSsR4Yp2g%3d&risl=&pid=ImgRaw&r=0", "https://th.bing.com/th/id/R.6ea993ea58f21c5797b97df59fd6f23b?rik=OFW4SWakgAdqqw&riu=http%3a%2f%2fmedia.new.mensxp.com%2fmedia%2fguylife%2fphotogallery%2f2012%2fSep%2fiamlegend.jpg&ehk=abmPVywrZDUoawcKqbCd7xYPAxbRHHqYKRr9LiYD3lQ%3d&risl=&pid=ImgRaw&r=0" ];
+
+for(var i=0;i<listaFilmes.length;i++){
+    document.write(" <img src = " + "      " +listaFilmes[i] +"      " +" >");
+}
+function anime(){
+    var valorElemento = document.getElementById("vai");
+
+
+var animes = ["https://th.bing.com/th/id/R.7e6fdab144199825359ff3116f1bfbac?rik=EiIA3qoEgBsXjA&riu=http%3a%2f%2f4.bp.blogspot.com%2f_3sU0MnRawMI%2fTShSRRXwFDI%2fAAAAAAAAD1A%2fIT9XCvTSPpc%2fs1600%2fnaruto%2525252Bsage%2525252Bmode%2525252B.jpg&ehk=i1Wf6fImNfJ5Y5byW5CcvsbzS3gI02ciky1VWtj4Hy0%3d&risl=&pid=ImgRaw&r=0", "https://i.pinimg.com/originals/8e/a6/db/8ea6dbca156be9bf34d38a52a36a8c38.jpg"]
+
+
+
+for(var i=0;i<animes.length;i++){
+   
+document.write( "<img src =  " + "      " +animes[i]+ "      "  +  ">");
+}
+
+}
